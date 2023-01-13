a912bd27a9da154461d0dacf14ab20fc3a1e0324
Md. Hasibur Rahman <hasibkyau.cse@gmail.com>
2023-01-13 15:43:16 +0600

    trying to make a local map

---
+++ 3D_Local_Map_Generator.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+import open3d as o3d
+import matplotlib.pyplot as plt
+import os
+import time
+
+folder_dir = "_out/"
+
+def read_ply(path):
+    cloud = o3d.io.read_point_cloud(path)
+    return cloud
+
+# create visualizer and window.
+vis = o3d.visualization.Visualizer()
+vis.create_window(height=480, width=640)
+
+# initialize pointcloud instance.
+pcd = (o3d.io.read_point_cloud("_out/00022637.ply"))
+pcd2 = (o3d.io.read_point_cloud("_out/00023351.ply"))
+
+# include it in the visualizer before non-blocking visualization.
+vis.add_geometry(pcd)
+
+# to add new points each dt secs.
+dt = 0.01
+previous_t = time.time()
+
+for pl in os.listdir(folder_dir):
+    time.sleep(0.02)
+    # check if the image ends with png
+    if (pl.endswith(".ply")):
+
+        if time.time() - previous_t > dt:
+            # Reading new points
+            path = folder_dir + pl
+            cloud = read_ply(path)
+            # o3d.visualization.draw_geometries([cloud])  # Visualize the point cloud
+
+            # extending with ndarrays.
+            # pcd.points.extend(np.asarray(cloud.points))
+
+            # Updating the points
+            pcd.points = cloud.points
+
+            # Updating the map
+            vis.update_geometry(pcd)
+            # vis.update_geometry(cloud)
+
+        # Ploting the extended points
+
+        vis.update_renderer()
+        vis.poll_events()
+vis.destroy_window()
